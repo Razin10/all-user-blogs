6739ef10574290fcc35b2f435752436697a651e0
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-11-01 19:31:27 +0600

    Update AddBlog.jsx

--- frontend/src/components/AddBlog.jsx
+++ frontend/src/components/AddBlog.jsx
@@ -10,7 +10,7 @@ const AddBlog = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://localhost:5000/api/blogs', {
+            const response = await axios.post('https://all-user-blogs-api.onrender.com/api/blogs', {
                 title,
                 content,
                 author,
